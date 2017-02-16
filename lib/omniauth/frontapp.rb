1cbaecc050c2ffc4cbfc03e1acc8e520e211d067
Marketa <marketa@nomnom.it>
2017-02-16 17:41:46 +0000

    uncomment unnecessary fns

--- lib/omniauth/frontapp.rb
+++ lib/omniauth/frontapp.rb
@@ -10,33 +10,19 @@ module OmniAuth
              site: 'https://app.frontapp.com',
              authorize_url: 'https://app.frontapp.com/oauth/authorize',
              token_url: 'https://app.frontapp.com/oauth/token'
-
-      uid { raw_info['id'] }
-
-      info do
-        {
-          email: raw_info['email'],
-        }
-      end
-
-      extra do
-        { 'raw_info' => raw_info }
-      end
-
-      def authorize_params
-        # for now override the scope here so that we can
-        # call /v1/me and get current users information
-        super.tap { |p| p[:response_type] = 'code' }
-      end
-
-      #idef callback_url
-        # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
-      #  options[:callback_url] || (full_host + script_name + callback_path)
+     
+      #def authorize_params
+      #  super.tap do |p|
+      #      p[:redirect_uri] = 'https://local.dev/auth/frontapp/callback'
+      #  end
       #end
-
-      def raw_info
-        @raw_info ||= access_token.get('me').parsed
-      end
+   
+      #def token_params
+      #  super.tap do |p|
+      #      p[:redirect_uri] = 'https://local.dev/auth/frontapp/callback'
+      #  end
+      #end
+       
     end
   end
 end
